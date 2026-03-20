050e8404085048e8ddef9d9c8836a519d862fb53
Vansh Gupta <vanshsja1512@gmail.com>
2026-03-20 20:43:53 +0530

    Update MainApp.java

--- Personal_Expense_Tracker/src/MainApp.java
+++ Personal_Expense_Tracker/src/MainApp.java
@@ -2,10 +2,8 @@ public class MainApp {
 
     public static void main(String[] args) 
     {
-
         ExpenseManager manager = new ExpenseManager(500);
         BudgetManager budgetManager = new BudgetManager();
-
         new DashboardFrame(manager, budgetManager);
     }
 }
