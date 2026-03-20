d2287794e4540b827e19ce9bd1f4ccdc153d5cfd
Vansh Gupta <vanshsja1512@gmail.com>
2026-03-20 20:38:59 +0530

    Categories

--- Personal_Expense_Tracker/src/CategoryReport.java
+++ Personal_Expense_Tracker/src/CategoryReport.java
@@ -1,3 +1,27 @@
-public class CategoryReport {
-    
+public class CategoryReport 
+{
+    String[] categories = {"Food", "Transport", "Shopping", "Bills", "Other"};
+    double[] totals = new double[categories.length];
+    public void calculateCategorySpending(Transaction[] arr, int count) 
+    {
+        for (int i = 0; i < totals.length; i++) 
+        {
+            totals[i] = 0;
+        }
+        for (int i = 0; i < count; i++) 
+        {
+            if (arr[i].getType().equals("EXPENSE")) 
+            {
+                for (int j = 0; j < categories.length; j++) 
+                {
+                    if (arr[i].getCategory().equals(categories[j])) 
+                    {
+                        totals[j] += arr[i].getAmount();
+                    }
+                }
+            }
+        }
+    }
+    public String[] getCategories() { return categories;}
+    public double[] getCategoryTotals() { return totals; }
 }
