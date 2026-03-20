ef790b10be98f09f036bc6798d8a85ec76e70813
Vansh Gupta <vanshsja1512@gmail.com>
2026-03-20 20:35:11 +0530

    Budget updater

--- Personal_Expense_Tracker/src/BudgetManager.java
+++ Personal_Expense_Tracker/src/BudgetManager.java
@@ -1,3 +1,20 @@
-public class BudgetManager {
-    
+public class BudgetManager 
+{
+    double monthlyBudget;
+    void setBudget(double amount) 
+    {
+        monthlyBudget = amount;
+    }
+    public double getBudget() 
+    {
+        return monthlyBudget;
+    }
+    public boolean isBudgetExceeded(double totalExpense) 
+    {
+        return totalExpense > monthlyBudget;
+    }
+    public double remainingBudget(double totalExpense) 
+    {
+        return monthlyBudget - totalExpense;
+    }
 }
