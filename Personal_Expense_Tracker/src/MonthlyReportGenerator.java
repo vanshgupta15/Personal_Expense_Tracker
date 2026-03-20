57f88294f5715f6f7aada190764acc3dcc4c046a
Vansh Gupta <vanshsja1512@gmail.com>
2026-03-20 20:44:22 +0530

    Monthly reports

--- Personal_Expense_Tracker/src/MonthlyReportGenerator.java
+++ Personal_Expense_Tracker/src/MonthlyReportGenerator.java
@@ -1,3 +1,31 @@
-public class MonthlyReportGenerator {
-    
+public class MonthlyReportGenerator 
+{
+    public double calculateMonthlyIncome(Transaction[] arr, int count, int month, int year) 
+    {
+        double total = 0;
+        for (int i = 0; i < count; i++) 
+        {
+            if (arr[i].getMonth() == month && arr[i].getYear() == year && arr[i].getType().equals("INCOME")) 
+            {
+                total += arr[i].getAmount();
+            }
+        }
+        return total;
+    }
+    public double calculateMonthlyExpense(Transaction[] arr, int count, int month, int year) 
+    {
+        double total = 0;
+        for (int i = 0; i < count; i++) 
+        {
+            if (arr[i].getMonth() == month && arr[i].getYear() == year && arr[i].getType().equals("EXPENSE")) 
+            {
+                total += arr[i].getAmount();
+            }
+        }
+        return total;
+    }
+    public double calculateMonthlyBalance(Transaction[] arr, int count, int month, int year) 
+    {
+        return calculateMonthlyIncome(arr, count, month, year) -calculateMonthlyExpense(arr, count, month, year);
+    }
 }
